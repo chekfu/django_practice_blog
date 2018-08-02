a6fbb278cd74b55ea3959d3339a95d3dcc345ef9
chekfu <chekfu20@gmail.com>
2018-08-02 14:30:17 +0200

    post_list view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,5 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 
@@ -33,3 +34,8 @@ def post_detail(request, year, month, day, post):
                   {'post': post})
 
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
