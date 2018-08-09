44b97ba4b8786895261ad8847b2516a76e824d24
chekfu <chekfu20@gmail.com>
2018-08-09 16:12:41 +0200

    tagging % filtering

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
+from taggit.models import Tag
 
 # Create your views here.
 
-def post_list(request):
+def post_list(request, tag_slug = None):
     object_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug = tag_slug)
+        object_list = object_list.filter(tags_in = [tag])
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -22,7 +27,8 @@ def post_list(request):
     return render(request,
                   'blog/post/list.html',
                   {'page': page,
-                   'posts': posts})
+                   'posts': posts,
+                   'tag': tag})
 
 
 def post_detail(request, year, month, day, post):
