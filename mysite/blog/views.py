b6350e4381c51c95ff568ece61262cb9b6e71d0c
chekfu <chekfu20@gmail.com>
2018-08-10 13:58:20 +0200

    tagging % filtering

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,11 @@
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
+from django.db.models import Count
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
-from django.core.mail import send_mail
-from taggit.models import Tag
-
 # Create your views here.
 
 def post_list(request, tag_slug = None):
@@ -13,7 +13,7 @@ def post_list(request, tag_slug = None):
     tag = None
     if tag_slug:
         tag = get_object_or_404(Tag, slug = tag_slug)
-        object_list = object_list.filter(tags_in = [tag])
+        object_list = object_list.filter(tags__in = [tag])
     paginator = Paginator(object_list, 3) # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -54,12 +54,20 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                .order_by('-same_tags','-publish')[:4]
     return render(request,
                   'blog/post/detail.html',
                   {'post': post,
                    'comments': comments,
                    'new_comment': new_comment,
-                   'comment_form': comment_form})
+                   'comment_form': comment_form,
+                   'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
