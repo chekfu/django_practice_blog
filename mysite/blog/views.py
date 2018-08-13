1dc8327ffed8476f0344ce4b2c1a47174bc4044f
chekfu <chekfu20@gmail.com>
2018-08-13 16:04:54 +0200

    search view added

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,10 +3,11 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
+from django.contrib.postgres.search import TrigramSimilarity
 # Create your views here.
 
 def post_list(request, tag_slug = None):
@@ -106,7 +107,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.objects.annotate(search=SearchVector('title','body'),).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body',weight='B')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.3).order_by('-similarity')
     return render(request,
                   'blog/post/search.html',
                   {'form': form,
