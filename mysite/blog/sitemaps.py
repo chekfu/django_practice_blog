e1b853ee123d909a74454557adf9608a6e8c0b99
chekfu <chekfu20@gmail.com>
2018-08-13 15:11:04 +0200

    switch to postgresql

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -4,7 +4,9 @@ from .models import Post
 class PostSitemap(Sitemap):
     changefreq = 'weekly'
     priority = 0.9
+
     def items(self):
         return Post.published.all()
+
     def lastmod(self, obj):
         return obj.updated
